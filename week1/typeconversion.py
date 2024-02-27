74ae0a3755eeccd495f7dcce7fb5704a11bdaaf0
MBEKE <ogadambeke@gmail.com>
2024-02-27 22:48:57 +0300

    Add: typeconversion.py file
    The program demonstrates implicit and explicit type conversions
    in Python

---
+++ week1/typeconversion.py
@@ -0,0 +1,16 @@
+# implicit conversion
+integer_number = 123
+float_number = 1.23
+new_number = integer_number + float_number
+#display new value and resulting data type
+print("Value:", new_number)
+print("Data Type:", type(new_number))
+# Output
+# explicit conversion
+num_string = '12'
+num_integer = 23
+num_string = int(num_string)
+print("Data type of num_string before Type Casting:",type(num_string))
+num_sum = num_integer + num_string
+print("Sum:",num_sum)
+print("Data type of num_sum:",type(num_sum))
