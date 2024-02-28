0208b740009eebfd41b241f0aa11bf4d29010cc1
MBEKE <ogadambeke@gmail.com>
2024-02-28 14:05:43 +0300

    Add: user_input.py file
    The program takes user input and dispays it on stdout.

---
+++ week1/user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter Your Name:")
+age = int(input("Enter Your Age:"))
+location = input("Your Loaction:")
+print("Here are your details")
+#f"Hello {name}. you are", {age}, years old and you live #in , {location}"
+print(f"Hello, {name}. You are {age} years old and live int {location}")
