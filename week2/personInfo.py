0b881d14ca921825cca9d0bd4200cc07ebd5ad6d
MBEKE <ogadambeke@gmail.com>
2024-03-03 18:34:42 +0300

    Add: personInfo.py file
    The program uses dictionary to store info about a person
    Takes user input and stores in the dictionary
    The program finally prints the dictionary to the console

---
+++ week2/personInfo.py
@@ -0,0 +1,10 @@
+# Create an empty dictionary to store information about a person
+person_info = {}
+# Ask the user for input and store info in the dictionary
+person_info['name'] = input("Enter Your Name: ")
+person_info['age'] = input("Enter your age: ")
+person_info['favourite_color'] = input("Enter your favourite color: ")
+
+# Print the dictionary to the console
+print("Your Info")
+print(person_info)
