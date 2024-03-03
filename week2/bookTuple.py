139a5bfcf9c65e8bab2066407aeeb82701e595ba
MBEKE <ogadambeke@gmail.com>
2024-03-03 17:48:11 +0300

    Add: bookTuple.py file
    The program creats a tuple of my 5 favourite books
    It uses for loop to print each book name on separete line

---
+++ week2/bookTuple.py
@@ -0,0 +1,5 @@
+# Create a tuple of my favourite books
+favouriteBooks =('River and The Source', 'The C Language', 'The Lean Business', 'Crosspath', 'Me Tomorrow',)
+# Iterate through the tuple
+for book in favouriteBooks:
+    print(f'My 5 favorite books are: {book}')
